f83861a9f1f177ba6a0ee35a2038ef29816abd1b
lame-over <dimanechkoq11@gmail.com>
2023-06-01 13:09:06 +0300

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1;
+            int sum;
+            Console.Write(n1+ " " + n2 + " ");
+            for (int i = 0; i <= 40; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+                Console.Write(sum + " ");
+            }
+        }
+    }
+}
